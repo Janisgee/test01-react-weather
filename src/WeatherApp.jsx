21d1d1f6be27ed3dc2fa3c985e6e813adfb78124
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-16 13:12:03 +0800

    Added Weather Structure

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./WeatherApp.css";
+
+export default function WeatherApp() {
+  return (
+    <div className="WeatherApp">
+      <div className="container weatherWrapper">
+        <h1>New York</h1>
+        <ul>
+          <li>Saturday 12:00 pm</li>
+          <li>Partly Cloudy</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix">
+              <div className="float-start">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                  alt="image"
+                />
+              </div>
+              <div className="float-start">
+                <span className="temperature">23</span>
+                <span className="units">°C | °F</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Sunrise: 05:35</li>
+              <li>Sunset: 17:40</li>
+              <li>Humidity: 77%</li>
+              <li>Wind: 11km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
