047247cc73a0347b5ed261c450759736bb986b10
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-19 15:47:37 +0800

    Finalized Weather App

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./WeatherApp.css";
+
 import SearchForm from "./SearchForm";
 
 export default function WeatherApp() {
@@ -7,6 +7,27 @@ export default function WeatherApp() {
     <div className="WeatherApp">
       <div className="container weatherWrapper">
         <SearchForm defaultCity="Hong Kong" />
+      </div>
+      <div className="openSourced">
+        This project was coded by{" "}
+        <a
+          href="http://www.linkedin.com/in/jc-janis-chan"
+          title="Janis's LinkedIn"
+        >
+          Janis Chan
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Janisgee" title="Janis's Github">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://app.netlify.com/teams/janisgee/overview"
+          title="Janis's Netlify"
+        >
+          hosted on Netlify
+        </a>
+        .
       </div>
     </div>
   );
